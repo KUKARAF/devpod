113066a2ca3e22aba7daf1705669f14d91a00b3e
Rafal Kukawski <rafal.kuka94@gmail.com>
2026-06-15 11:15:12 +0200

    Fetch latest zellij version dynamically instead of hardcoding

--- files/system/usr/libexec/install-zellij.sh
+++ files/system/usr/libexec/install-zellij.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
-VERSION="0.44.3"
-DOWNLOAD_URL="https://github.com/zellij-org/zellij/releases/download/v${VERSION}/zellij-x86_64-unknown-linux-musl.tar.gz"
 INSTALL_DIR="/usr/local/bin"
+
+echo "Fetching latest zellij release..."
+VERSION=$(curl -s https://api.github.com/repos/zellij-org/zellij/releases/latest | grep '"tag_name"' | cut -d'"' -f4 | sed 's/^v//')
+DOWNLOAD_URL="https://github.com/zellij-org/zellij/releases/download/v${VERSION}/zellij-x86_64-unknown-linux-musl.tar.gz"
 
 echo "Installing zellij v${VERSION}..."
 cd /tmp
@@ -11,4 +13,4 @@ curl -sL "$DOWNLOAD_URL" -o zellij.tar.gz
 tar xzf zellij.tar.gz
 install -m 755 zellij "$INSTALL_DIR/zellij"
 rm -f zellij.tar.gz zellij
-echo "Zellij installed to $INSTALL_DIR/zellij"
+echo "Zellij v${VERSION} installed to $INSTALL_DIR/zellij"
